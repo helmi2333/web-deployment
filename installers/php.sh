528088205cbdae6180fd3479093f514161a510e5
Kevin Burke <kev@inburke.com>
2016-01-05 21:36:17 -0800

    Add PHP installation script

--- installers/php.sh
+++ installers/php.sh
@@ -6,6 +6,13 @@ main() {
   local version='5.6.16'
   local directory="php-$version"
   local binary="$directory.tar.bz2"
+  local prefix=/home/php
+
+  if [[ ! -d "$prefix" ]]; then
+      echo "$prefix does not exist; please create it" 1>&2
+      exit 1
+  fi
+
   mkdir -p $HOME/src
   pushd $HOME/src
     if [[ ! -f "$binary" ]]; then
@@ -19,7 +26,8 @@ main() {
     pushd "$directory"
         ./configure --enable-pcntl --with-curl --with-mcrypt --enable-mbstring \
             --with-openssl --enable-fpm --with-mysql --with-libedit \
-            --with-libdir=/lib/x86_64-linux-gnu
+            --with-libdir=/lib/x86_64-linux-gnu \
+            --prefix="$prefix"
         make
         make install
     popd
