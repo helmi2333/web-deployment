97d1004d9c1c1ad3c83b45ceffe575c9d34292b6
Kevin Burke <kev@inburke.com>
2016-01-18 23:21:06 -0800

    fix up database user creation scripts

--- scripts/restore_fsm_database.bash
+++ scripts/restore_fsm_database.bash
@@ -6,8 +6,7 @@ main() {
     local current_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
     local fsm_password=$(cat "$current_directory/../passwords/mysql/fsm.password")
     ssh wildebeest-root mysql -uroot < "$current_directory/../databases/fsm.dump"
-    ssh wildebeest-root mysql --execute "CREATE USER kevinburke_fsm IDENTIFIED BY \"$fsm_password\" ;"
-    ssh wildebeest-root mysql --execute "GRANT ALL PRIVILEGES ON kevinburke_fsm.* TO kevinburke_fsm"
+    ssh wildebeest-root "mysql --execute 'GRANT ALL ON kevinburke_fsm.* to kevinburke_fsm@localhost identified by \"226ab754\";'"
 }
 
 main "$@"
