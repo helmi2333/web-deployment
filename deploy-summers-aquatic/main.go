7a50bf594ac99aba9fb7a989e05730c9f2e537a6
Kevin Burke <kev@inburke.com>
2016-12-11 20:45:39 -0800

    move code into other path

--- deploy-summers-aquatic/main.go
+++ deploy-summers-aquatic/main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"io/ioutil"
 	"log"
+	"path/filepath"
 	"time"
 
 	"github.com/kevinburke/ansible-go/core"
@@ -19,6 +20,36 @@ type Config struct {
 
 const groupname = "summersaquatic"
 const username = "summersaquatic"
+const wpVersion = "4.6.1"
+const wpRefspec = "remotes/origin/4.6-branch"
+
+func Provision(ctx context.Context) error {
+	if err := core.AddGroup(ctx, groupname, core.GroupOpts{}); err != nil {
+		return err
+	}
+	if err := user.Add(ctx, username, user.Group(groupname), user.AppendGroups(), user.Shell("/bin/bash")); err != nil {
+		return err
+	}
+	// TODO pull this from user.Add().Homedir?
+	homedir := filepath.Join("/home", username)
+	dirOpts := core.DirOpts{Mode: 0755}
+	if err := core.CreateDirectory(ctx, filepath.Join(homedir, ".ssh"), dirOpts); err != nil {
+		return err
+	}
+	if err := core.CreateDirectory(ctx, filepath.Join(homedir, "public"), dirOpts); err != nil {
+		return err
+	}
+	if err := core.CreateDirectory(ctx, filepath.Join(homedir, "public", ".well-known"), dirOpts); err != nil {
+		return err
+	}
+	err := core.Git(ctx, "https://github.com/WordPress/WordPress.git",
+		filepath.Join(homedir, "wordpress"),
+		core.GitOpts{Version: wpVersion, Refspec: wpRefspec, Depth: 1})
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 func main() {
 	cfg := flag.String("config", "config.yml", "Path to a config file")
@@ -37,10 +68,8 @@ func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), *dur)
 	defer cancel()
 
-	if err := core.AddGroup(ctx, groupname, core.GroupOpts{}); err != nil {
+	if err := Provision(ctx); err != nil {
 		log.Fatal(err)
 	}
-	if err := user.Add(ctx, username, user.Group(groupname), user.AppendGroups(), user.Shell("/bin/bash")); err != nil {
-		log.Fatal(err)
-	}
+	log.Fatal("foo")
 }
